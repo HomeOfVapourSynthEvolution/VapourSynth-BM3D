77082233f79b9fe5a84d0cb11bf972de6e4272d5
mawen1250 <mawen1250@gmail.com>
2015-11-18 22:51:11 +0800

    Added SSE4.1 implement of hard-thresholding filter for basic estimate

--- source/VBM3D_Basic.cpp
+++ source/VBM3D_Basic.cpp
@@ -18,6 +18,9 @@
 
 
 #include "VBM3D_Basic.h"
+#if defined(USE_SSE4_1)
+#include <smmintrin.h>
+#endif
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -82,6 +85,35 @@ void VBM3D_Basic_Process::CollaborativeFilter(int plane,
     auto srcp = srcGroup.data();
     auto thrp = d.f[plane].thrTable[GroupSize - 1].get();
     const auto upper = srcp + srcGroup.size();
+
+#if defined(USE_SSE4_1)
+    static const ptrdiff_t step = 4;
+    const ptrdiff_t residue = srcGroup.size() % step;
+    const ptrdiff_t simd_width = srcGroup.size() - residue;
+
+    static const __m128 zero_ps = _mm_setzero_ps();
+    __m128i cmp_sum = _mm_setzero_si128();
+
+    for (const auto upper1 = srcp + simd_width; srcp < upper1; srcp += step, thrp += step)
+    {
+        _mm_prefetch(reinterpret_cast<const char *>(srcp), _MM_HINT_NTA);
+        _mm_prefetch(reinterpret_cast<const char *>(thrp), _MM_HINT_NTA);
+
+        const __m128 s1 = _mm_load_ps(srcp);
+        const __m128 t1p = _mm_load_ps(thrp);
+        const __m128 t1n = _mm_sub_ps(zero_ps, t1p);
+
+        const __m128 cmp1 = _mm_cmpgt_ps(s1, t1p);
+        const __m128 cmp2 = _mm_cmplt_ps(s1, t1n);
+        const __m128 cmp = _mm_or_ps(cmp1, cmp2);
+
+        const __m128 d1 = _mm_blendv_ps(zero_ps, s1, cmp);
+        _mm_store_ps(srcp, d1);
+        cmp_sum = _mm_sub_epi32(cmp_sum, _mm_castps_si128(cmp));
+    }
+
+    retainedCoefs += cmp_sum.m128i_i32[0] + cmp_sum.m128i_i32[1] + cmp_sum.m128i_i32[2] + cmp_sum.m128i_i32[3];
+#endif
 
     for (; srcp < upper; ++srcp, ++thrp)
     {
